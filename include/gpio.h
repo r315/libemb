e6c86127d134123de8cf9df16e6eae975b78a60c
r315 <38652@alunos.isel.ipl.pt>
2018-04-22 02:03:27 +0100

    ADD: GPIO_OUTPUT_SET

--- include/gpio.h
+++ include/gpio.h
@@ -152,6 +152,9 @@ void GPIO_ResetPINSEL(void);
 
 #elif defined(__ESP03__)
 
+#define GPIO_OUTPUT_SET(gpio_no, bit_value) \
+gpio_output_set((bit_value)<<gpio_no, ((~(bit_value))&0x01)<<gpio_no, 1<<gpio_no,0)
+
 #else
 #error "Define Board"
 
