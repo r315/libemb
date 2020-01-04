d58a371cdba6ea8524feff90ea50752e600fd9e1
r315 <38652@alunos.isel.ipl.pt>
2020-01-04 10:25:26 +0000

    Fix data types on functions declaration

--- misc/debug.c
+++ misc/debug.c
@@ -1,34 +1,45 @@
+
+#include <stdint.h>
+#if 0
 #include <display.h>
+#define DBG_PRINTF	DISPLAY_printf
+#define DBG_PUTC	DISPLAY_putc
+
+#else
+#include <stdio.h>
+#define DBG_PRINTF	printf
+#define DBG_PUTC	putchar
+#endif
 
 #define LINESIZE 0x8
 
-
-void hexDumpLine(char *mem, char print_ascii, int len){
+void hexDumpLine(uint8_t *mem, uint8_t print_ascii, uint32_t len){
 int i;	
 
     for(i=0; i<len; i++){
-		DISPLAY_printf("%02X",*(mem + i));
+		DBG_PRINTF("%02X ",*(mem + i));
 	}
+
     if(!print_ascii)
         return;
 
 	for(i=0;i<len;i++){
 		if(*mem > (' '-1) && *mem < 0x7F)
-			DISPLAY_printf("%c",*mem);
+			DBG_PRINTF("%c",*mem);
 		else{
-			DISPLAY_putc('.');
+			DBG_PUTC('.');
 		}
 		mem++;
 	}
 }
 
 
-void hexDump(char *mem, int len){
-    DISPLAY_printf("%p:\n",mem);
+void hexDump(uint8_t *mem, uint32_t len){
+    DBG_PRINTF("\n Dump address: 0x%p\n\n",mem);
 	for(int i=0; i<len ;i+=LINESIZE){
-		DISPLAY_printf("%02X: ",i);
+		DBG_PRINTF("%02X: ",i);
 		hexDumpLine(mem, 1, LINESIZE);
-		DISPLAY_putc('\n');
+		DBG_PUTC('\n');
 		mem += LINESIZE;
 	}
 }
