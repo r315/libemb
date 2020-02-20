6dabf65f8a14bed2f8554ec45d09b20f70b3305b
r315 <38652@alunos.isel.ipl.pt>
2020-02-20 23:38:15 +0000

    updated debug api to support stdout structure

--- include/dbg.h
+++ include/dbg.h
@@ -1,9 +1,11 @@
 #ifndef _DBG_H_
 #define _DBG_H_
 #include <stdint.h>
+#include <stdout.h>
 
-void dbgHexDump(uint8_t *mem, uint32_t len);
-void dbgHexDumpLine(uint8_t *mem, uint32_t len, uint8_t print_ascii);
+void dbg_HexDump(uint8_t *mem, uint32_t len);
+void dbg_HexDumpLine(uint8_t *mem, uint32_t len, uint8_t print_ascii);
 void dbg_printf(const char*, ...);
 void dbg_puts(char*);
+void dbg_init(stdout_t *stdo);
 #endif /* _DBG_H_ */
--- misc/debug.c
+++ misc/debug.c
@@ -6,11 +6,32 @@
 #include <strfunc.h>
 #include <dbg.h>
 
-extern void dbg_putc(char c);
-
 #define LINESIZE 0x8
 
-void dbgHexDumpLine(uint8_t *mem, uint32_t len, uint8_t print_ascii ){
+void dummy_init(void){ }
+char dummy_getchar(void){ return 255; }
+void dummy_putchar(char c){ }
+void dummy_puts(const char *str){ }
+uint8_t dummy_nb(char *c){ return 0; }
+uint8_t dummy_kbhit(void){ return 0;}
+
+ static stdout_t dummy_out = {
+	dummy_init,
+	dummy_getchar,
+	dummy_putchar,
+	dummy_puts,
+	dummy_nb,
+	dummy_kbhit
+};
+static stdout_t *sto = &dummy_out;
+
+void dbg_init(stdout_t *stdo){
+	if(stdo != NULL){
+		sto = stdo;
+	}	
+}
+
+void dbg_HexDumpLine(uint8_t *mem, uint32_t len, uint8_t print_ascii ){
 int i;	
 
     for(i=0; i<len; i++){
@@ -25,27 +46,25 @@ int i;
 		if(*mem > (' '-1) && *mem < 0x7F)
 			dbg_printf("%c", *mem);
 		else{
-			dbg_putc('.');
+			sto->xputchar('.');
 		}
 		mem++;
 	}
-	dbg_putc('\n');
+	sto->xputchar('\n');
 }
 
 
-void dbgHexDump(uint8_t *mem, uint32_t len){
+void dbg_HexDump(uint8_t *mem, uint32_t len){
     dbg_printf("\n Dump address: 0x%p\n\n",mem);
 	for(int i=0; i<len ;i+=LINESIZE){
 		dbg_printf("%02X: ",i);
-		dbgHexDumpLine(mem, LINESIZE, 1);		
+		dbg_HexDumpLine(mem, LINESIZE, 1);		
 		mem += LINESIZE;
 	}
 }
 
-void dbg_puts(char *str){
-	while(*str){
-		dbg_putc(*str++);
-	}
+void dbg_puts(char *str){	
+	sto->xputs(str);
 }
 
 void dbg_printf(const char* str, ...)
@@ -57,7 +76,7 @@ void dbg_printf(const char* str, ...)
 
 	while ((d = *str++) != 0) {
 		if (d != '%') {
-			dbg_putc(d); continue;
+			sto->xputchar(d); continue;
 		}
 		d = *str++; f = w = r = s = l = 0;
 		if (d == '.') {
@@ -76,11 +95,11 @@ void dbg_printf(const char* str, ...)
 		}
 		if (!d) break;
 		if (d == 's') {
-			dbg_puts(va_arg(arp, char*));
+			sto->xputs(va_arg(arp, char*));
 			continue;
 		}
 		if (d == 'c') {
-			dbg_putc((char)va_arg(arp, int));
+			sto->xputchar((char)va_arg(arp, int));
 			continue;
 		}
 		if (d == 'u') r = 10;
@@ -90,19 +109,19 @@ void dbg_printf(const char* str, ...)
 		if (d == 'f') {
 			if (!f)
 				w = 6;						// dafault 6 decimal places
-			dbg_puts(pftoa(va_arg(arp, double), w));
+			sto->xputs(pftoa(va_arg(arp, double), w));
 			continue;
 		}
 		if (!r) break;
 		if (s) w = -w;
 		if (l) {
-			dbg_puts(pitoa((long)va_arg(arp, long), r, w));
+			sto->xputs(pitoa((long)va_arg(arp, long), r, w));
 		}
 		else {
 			if (r > 0)
-				dbg_puts(pitoa((unsigned long)va_arg(arp, int), r, w));
+				sto->xputs(pitoa((unsigned long)va_arg(arp, int), r, w));
 			else
-				dbg_puts(pitoa((long)va_arg(arp, int), r, w));
+				sto->xputs(pitoa((long)va_arg(arp, int), r, w));
 		}
 	}
 
