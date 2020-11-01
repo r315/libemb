f9161e05dacb103feac8badf8b0c0cdb46f09fc4
r315 <38652@alunos.isel.ipl.pt>
2020-11-01 23:32:15 +0000

    added option for defining data width

--- tools/f2ba.c
+++ tools/f2ba.c
@@ -2,26 +2,45 @@
 #include <stdlib.h>
 #include <string.h>
 #include <stdint.h>
+#include <ctype.h>
 
 /**
  * File to byte Array
  * 
  * */
 
-#define NCOLS 32
+#define NCOLS 16
 
 typedef struct{
     int offset;
     char *infile;
     char *outfile;
+    int data_size;
 }Arguments;
+
+const char *data_types[] = {"char", "uint8_t", "uint16_t", "uint32_t", "uint64_t"};
 
 void help(){
     printf("File to byte array ");
-    printf("Usage: f2ba <-if input file> <-of output file> [-ofs <offset>]\n");
+    printf("Usage: f2ba <-if input file> <-of output file> [-ofs <offset>] [-ds <bytes>]\n");
 }
 
-void savetofile(char *filename, uint8_t *buf, int len){
+#ifndef _WIN32
+
+char *strupr(char *str){
+    char *ptr = str;
+    while(*str){
+        if(!(*str < 'a') && !(*str > 'z')){
+            *str = toupper(*str);
+        }
+        str++;
+    }
+    return ptr;
+}
+
+#endif
+
+void savetofile(char *filename, uint8_t *buf, int len, int data_size){
 FILE *fp;
 char namecpy[20];
 
@@ -37,17 +56,21 @@ char namecpy[20];
 
     //fprintf(fp,"#if 0\n%.*s\n#endif\n", len, buf); // print file content if printable chars??
     strcpy(namecpy, filename);
-    fprintf(fp,"#define %s_SIZE    %d\n", strupr(namecpy), len);
-    fprintf(fp,"const char %s[] = {", filename);
+    fprintf(fp,"#define %s_SIZE    %d\n", strupr(namecpy), len/data_size);
+    fprintf(fp,"const %s %s[] = {", data_types[data_size], filename);
 
-    for(int i = 0; i < len; i++){
+    for(int i = 0; i < len; ){
         if((i % NCOLS) == 0){
             fprintf(fp,"\n");
         }
         
-        fprintf(fp,"0x%02X", buf[i]);
+        fprintf(fp,"0x%02X", buf[i++]);
 
-        if(i < len - 1){
+        for(int ds = 1; ds < data_size; ds++){
+            fprintf(fp,"%02X", buf[i++]);
+        }
+
+        if(i < len ){
             fprintf(fp,",");
         }
     }
@@ -61,6 +84,7 @@ static void parseCmdl(int ac, char** av, Arguments* arg){
     arg->offset = 0;
     arg->infile = NULL;
     arg->outfile = NULL;
+    arg->data_size = 1;
 
      /* parse options in any order */
     for (int pos = 1; pos < ac && av[pos][0] == '-'; pos++)
@@ -78,6 +102,11 @@ static void parseCmdl(int ac, char** av, Arguments* arg){
         if (!strcmp(av[pos], "-ofs"))
         {
             arg->offset = atoi(av[++pos]);
+        }
+
+        if (!strcmp(av[pos], "-ds"))
+        {
+            arg->data_size = atoi(av[++pos]);
         }
     }
 
@@ -101,6 +130,7 @@ Arguments args;
 
     if(fp == NULL){
         fprintf(stderr,"Unable to open file '%s'\n", filename);
+        help();
         exit(-1);
     }
 
@@ -127,7 +157,7 @@ Arguments args;
 
     fclose(fp);
 
-    savetofile(args.outfile, (uint8_t*)buf, size);
+    savetofile(args.outfile, (uint8_t*)buf, size, args.data_size);
 
     free(buf);
 
