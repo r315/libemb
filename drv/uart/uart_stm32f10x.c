79836bca9b7145c573010ab8797c9e9842a78f7b
r315 <38652@alunos.isel.ipl.pt>
2021-11-29 00:07:20 +0000

    Update uart driver for stm32f103

---
+++ drv/uart/uart_stm32f10x.c
@@ -0,0 +1,170 @@
+#include "uart.h"
+#include "stm32f1xx.h"
+
+#define GPIO_IN_PU   ((2 << 2)|(0 << 0))  // CNF | MODE
+#define GPIO_OUT_AF  ((2 << 2)|(2 << 0))
+
+static void setBaudrate(serialbus_t *huart)
+{
+    USART_TypeDef *usart = (USART_TypeDef*)huart->ctrl;
+    
+    float uartdiv = (float)(SystemCoreClock/huart->speed) / 32;
+
+    if(usart == USART1){
+        uartdiv *= 2;
+    }
+
+    uint16_t mantissa = (int)uartdiv;
+    uartdiv -= mantissa;
+    uint8_t frac = uartdiv * 16;
+
+    usart->BRR = (mantissa << 4) | frac;
+}
+
+void UART_Init(serialbus_t *serialbus){
+    USART_TypeDef *usart = NULL;
+	IRQn_Type irq;
+    
+    switch(serialbus->bus){
+
+    case UART_BUS1:
+        RCC->APB2ENR |= RCC_APB2ENR_USART1EN;
+        asm("nop");
+        RCC->APB2RSTR |= RCC_APB2RSTR_USART1RST;
+        asm("nop");
+        RCC->APB2RSTR &= ~RCC_APB2RSTR_USART1RST;
+
+#ifdef UART_DMA
+        RCC->AHBENR |= RCC_AHBENR_DMA1EN;
+        DMA1_Channel4->CCR = 0;
+        DMA1_Channel4->CPAR = (uint32_t)&USART1->DR;
+        //DMA1_Channel4->CMAR = (uint32_t)tx_buf;
+        DMA1_Channel4->CNDTR = 0;        
+#endif
+        /**
+         * PA9 -> TX
+         * PA10 <- RX
+         * */
+        GPIOA->CRH = (GPIOA->CRH & ~(0xFF << 4)) | (GPIO_IN_PU << 8) | (GPIO_OUT_AF << 4);
+        GPIOA->BSRR = GPIO_PIN_10;
+
+        usart = USART1;
+        irq = USART1_IRQn;
+        break;
+    
+    case UART_BUS2:
+        RCC->APB1ENR |= RCC_APB1ENR_USART2EN;
+        asm("nop");
+        RCC->APB1RSTR |= RCC_APB1RSTR_USART2RST;
+        asm("nop");
+        RCC->APB1RSTR &= ~RCC_APB1RSTR_USART2RST;
+
+        /**
+         * PA2 -> TX
+         * PA3 <- RX
+         * */
+        GPIOA->CRL = (GPIOA->CRL & ~(0xFF << 8)) | (GPIO_IN_PU << 12) | (GPIO_OUT_AF << 8);
+        GPIOA->BSRR = GPIO_PIN_3;
+
+        usart = USART2;
+        irq = USART1_IRQn;
+        break;
+
+    case UART_BUS3:
+        RCC->APB1ENR |= RCC_APB1ENR_USART3EN;
+        asm("nop");
+        RCC->APB1RSTR |= RCC_APB1RSTR_USART3RST;
+        asm("nop");
+        RCC->APB1RSTR &= ~RCC_APB1RSTR_USART3RST;
+
+        /**
+         * PB10 -> TX
+         * PB11 <- RX
+         * */
+        GPIOB->CRH = (GPIOB->CRH & ~(0xFF << 8)) | (GPIO_IN_PU << 12) | (GPIO_OUT_AF << 8);
+        GPIOB->BSRR = GPIO_PIN_11;
+        
+        usart = USART1;
+        irq = USART1_IRQn;
+        break;
+    
+    default:
+        return;
+    }
+
+    
+    serialbus->ctrl = usart;
+
+    setBaudrate(serialbus);
+
+    usart->CR1 = USART_CR1_TE | USART_CR1_RE | USART_CR1_UE;
+    usart->CR1 |= USART_CR1_RXNEIE;
+
+    NVIC_EnableIRQ(irq);
+}
+
+void UART_PutChar(serialbus_t *huart, char c){
+    USART_TypeDef *usart = (USART_TypeDef*)huart->ctrl;
+
+    if(fifo_put(&huart->txfifo, c))
+    usart->CR1 |= USART_CR1_TXEIE;
+}
+
+void UART_Puts(serialbus_t *huart, const char* str){
+    USART_TypeDef *usart = (USART_TypeDef*)huart->ctrl;
+
+    while(*str){
+        fifo_put(&huart->txfifo, *(uint8_t*)str++);
+    }
+    
+    usart->CR1 |= USART_CR1_TXEIE;
+}  
+
+char UART_GetChar(serialbus_t *huart){
+    uint8_t c;
+    fifo_get(&huart->rxfifo, &c);
+    return (char)c;
+}
+
+uint8_t UART_GetCharNonBlocking(serialbus_t *huart, char *c){
+    if(fifo_avail(&huart->rxfifo))
+        return fifo_get(&huart->rxfifo, (uint8_t*)c);
+    return 0;
+}
+
+uint8_t UART_Kbhit(serialbus_t *huart){
+    return fifo_avail(&huart->rxfifo);
+}
+
+void UART_IRQHandler(void *ptr){
+    serialbus_t *serialbus;
+    USART_TypeDef *usart; 
+    
+    if(ptr == NULL){
+        return;
+    }
+    
+    serialbus = (serialbus_t*)ptr;
+    usart = (USART_TypeDef*)serialbus->ctrl;
+    volatile uint32_t status = usart->SR;
+
+    // Data received
+    if (status & USART_SR_RXNE) {
+        usart->SR &= ~USART_SR_RXNE;
+        fifo_put(&serialbus->rxfifo, (uint8_t)usart->DR);
+    }
+    
+    // Check if data transmiter if empty 
+    if (status & USART_SR_TXE) {
+        usart->SR &= ~USART_SR_TXE;	          // clear interrupt
+        // Check if data is available to send
+        if(fifo_avail(&serialbus->txfifo) > 0){
+            uint8_t data;
+            fifo_get(&serialbus->txfifo, &data);
+            usart->DR = data;            
+        }else{
+               // No more data, disable interrupt
+            usart->CR1 &= ~USART_CR1_TXEIE;		      // disable TX interrupt if nothing to send
+        }
+    }    
+}
