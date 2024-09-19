f7d0262fc8bac319584cbd9647b5dd2da947dc16
Hugo Reis <38652@alunos.isel.ipl.pt>
2024-09-19 12:27:44 +0100

    FIX: wrong calculation of samples number for sine wave generator.
    The number of samples is defined by the desired frequency and sample rate,
    not by user input.
    Added option output to file

--- tools/sine_gen.js
+++ tools/sine_gen.js
@@ -1,64 +1,89 @@
 const fs = require('fs')
 
 
-function genBlockFile(outfile, freq, points){
-    const writeStream = fs.createWriteStream(outfile)
-    const pathName = writeStream.path
+function genBlockFile(freq, sr, outfile){
+    let writeStream
+    let pathName
     let values = []
+    let rows = []
     let line = ""
 
-    writeStream.on('finish', () => {
-        console.log(`wrote all the array data to file ${pathName}`);
-    })
-    
-    // handle the errors on the write process
-    writeStream.on('error', (err) => {
-        console.error(`There is an error writing the file ${pathName} => ${err}`)
-    })
-
-
-    console.log(`Frequency ${freq}`)
-    console.log(`points ${points}`)
-
-    let t = (1/freq) / points
     let amplitude = 0x7FFF
 
-    for(let x = 0, p = 0; x < (1/freq) - t; x += t, p++)
+
+    let nsamples = sr / freq
+
+    let t = (1 / freq) / nsamples     // time between samples
+
+    console.log(`Time ${t}`)
+    console.log(`Samples ${nsamples}`)
+
+    for(let x = 0, p = 0; p < nsamples; x += t, p++)
     {
-        if((p % 10) == 0){
-            values.push(line)
+        if((p % 10) == 0 && !outfile){
+            rows.push(line)
             console.log(line)
             line = ""
         }
+
         let val = Math.sin(2 * Math.PI * freq * x)
         //val += Math.sin(2 * Math.PI * (freq  * 2) * x)
-        
+
         val = Math.floor(amplitude * val)
-        
+
         let hex = (val & 0xFFFF).toString(16)
-        line += "0x" + ("0000" + hex).slice(-4)  + ", "
+        hex = ("0000" + hex).slice(-4)
+        values.push(hex)
+        line += "0x" + hex  + ", "
         //line += val.toString() + ", "
-        //values.push(val)
-        //values.push(val.toFixed(5))
+        //rows.push(val)
+        //rows.push(val.toFixed(5))
     }
 
-    values.push(line)
+    rows.push(line)
+
+    if(!outfile){
+        console.log(line)
+    }
+
+    /* Output for verilog memory initialization
+    line = ""
+
+    values.slice().reverse().forEach(x => {
+        line += x;
+        if (line.length == 64){
+            console.log(line)
+            line = ""
+        }
+    })
+
     console.log(line)
+    */
 
-    //fs.writeFile(outfile, values, (err) => {if(err)console.log(err)})
+    if(outfile) {
+        writeStream = fs.createWriteStream(outfile)
+        pathName = writeStream.path
 
-    values.forEach(value => writeStream.write(`${value}\n`))
-    writeStream.end()
+        writeStream.on('finish', () => {
+            console.log(`wrote all the array data to file ${pathName}`);
+        })
+
+        // handle the errors on the write process
+        writeStream.on('error', (err) => {
+            console.error(`There is an error writing the file ${pathName} => ${err}`)
+        })
+
+        rows.forEach(value => writeStream.write(`${value}\n`))
+        writeStream.end()
+    }
 }
 
 let args = process.argv.slice(2);
 
-if(args < 2){
-    console.log(`usage: sine_gen <freq> <samples>`)
+if(args.length < 2){
+    console.log(`usage: sine_gen <freq> <sample rate> [file]`)
+    console.log("Generate ONE Cycle for given sample rate")
     process.exit(0)
 }
 
-let freq = args[0]
-let points = args[1]
-
-genBlockFile('sine.c', freq, points)
+genBlockFile(args[0], args[1], args[2])
