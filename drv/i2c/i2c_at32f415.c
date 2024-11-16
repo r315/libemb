ea76b7b2aac94dc5c045d8fae303433b29ad275f
r315 <38652@alunos.isel.ipl.pt>
2024-11-16 13:13:09 +0000

    CHG: Configure I2C gpios on I2C init

--- drv/i2c/i2c_at32f415.c
+++ drv/i2c/i2c_at32f415.c
@@ -12,22 +12,54 @@
 #include "at32f4xx.h"
 #include "i2c.h"
 
+static void i2c_gpio_init(i2cbus_t *i2cbus)
+{
+    switch (i2cbus->bus_num){
+        case I2C_BUS0:
+            RCC->APB2EN |= RCC_APB2EN_GPIOBEN;
+            GPIOB->CTRLL = (GPIOB->CTRLL & ~(0xFF << 24)) | (0xAA << 24); // PB6/PB7 GPO_AF_PP
+            break;
+
+        case I2C_BUS1:
+            RCC->APB2EN |= RCC_APB2EN_GPIOBEN;
+            GPIOB->CTRLH = (GPIOB->CTRLH & ~(0xFF << 8)) | (0xAA << 8); // PB10/PB11
+            break;
+
+        case I2C_BUS2:
+            RCC->APB2EN |= RCC_APB2EN_AFIOEN | RCC_APB2EN_GPIOBEN;
+            AFIO->MAP5 = (AFIO->MAP & ~(7 << 4)) | AFIO_MAP5_I2C1_GRMP_01;
+            GPIOB->CTRLH = (GPIOB->CTRLH & ~(0xFF << 0)) | (0xAA << 0); // PB8/PB9
+            break;
+
+        case I2C_BUS3:
+            RCC->APB2EN |= RCC_APB2EN_AFIOEN | RCC_APB2EN_GPIOFEN;
+            AFIO->MAP5 = (AFIO->MAP & ~(7 << 8)) | AFIO_MAP5_I2C2_GRMP_11;
+            GPIOF->CTRLL = (GPIOF->CTRLL & ~(0xFF << 24)) | (0xAA << 24); // PF6/PF7
+            break;
+
+        default:
+            break;
+    }
+}
+
 void I2C_Init (i2cbus_t *i2cbus){
     I2C_Type *i2c;
     I2C_InitType  init;
 
 	switch(i2cbus->bus_num){
 		case I2C_BUS0:
+        case I2C_BUS2:
             RCC->APB1EN |= RCC_APB1EN_I2C1EN;
             i2c = I2C1;
             break;
 
         case I2C_BUS1:
+        case I2C_BUS3:
             RCC->APB1EN |= RCC_APB1EN_I2C2EN;
             i2c = I2C2;
             break;
 
-		default:	
+		default:
 			return;
 	}
 
@@ -43,6 +75,8 @@ void I2C_Init (i2cbus_t *i2cbus){
     I2C_Cmd(i2c, ENABLE);
 
     I2C_Initialize(i2c, &init);
+
+    i2c_gpio_init(i2cbus);
 }
 
 void static i2c_reset_on_error(i2cbus_t *i2cbus, I2C_StatusType lastres){
@@ -62,6 +96,7 @@ void static i2c_reset_on_error(i2cbus_t *i2cbus, I2C_StatusType lastres){
         }
     }
 }
+
 uint32_t I2C_Write(i2cbus_t *i2cbus, uint8_t *data, uint32_t size){
     I2C_StatusType res = I2C_Master_Transmit(i2cbus->peripheral, i2cbus->addr, data, size, 1000);
 
@@ -69,7 +104,7 @@ uint32_t I2C_Write(i2cbus_t *i2cbus, uint8_t *data, uint32_t size){
         i2c_reset_on_error(i2cbus, res);
         return 0;
     }
-    
+
     return size;
 }
 
@@ -80,7 +115,7 @@ uint32_t I2C_Read(i2cbus_t *i2cbus, uint8_t *data, uint32_t size){
         i2c_reset_on_error(i2cbus, res);
         return 0;
     }
-    
+
     return size;
 }
 
