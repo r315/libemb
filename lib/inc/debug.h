0a665120af4062cfe819083689149a39489fba33
r315 <38652@alunos.isel.ipl.pt>
2025-08-11 10:17:57 +0100

    FIX: unused variable warning

--- lib/inc/debug.h
+++ lib/inc/debug.h
@@ -12,12 +12,12 @@ extern "C" {
 #define DBG_PRINT_MAX_LEN   64
 #endif
 
-void dbg_HexDump(uint8_t *mem, uint32_t len);
-void dbg_HexDumpLine(uint8_t *mem, uint32_t len, uint8_t print_ascii);
-void  dbg_putchar(char);
+void dbg_HexDump(const uint8_t *mem, uint32_t len);
+void dbg_HexDumpLine(const uint8_t *mem, uint32_t len, uint8_t print_ascii);
+void dbg_putchar(char);
 int  dbg_printf(const char*, ...);
-int  dbg_puts(const char*);
-void dbg_init(stdinout_t *);
+int  dbg_println(const char*);
+void dbg_init(const stdinout_t *);
 
 #ifdef __cplusplus
 }
--- lib/src/debug.c
+++ lib/src/debug.c
@@ -1,5 +1,5 @@
 
-#ifdef DEBUG
+#if defined(DEBUG) || defined(ENABLE_DEBUG)
 #include <stdint.h>
 #include <stdio.h>
 #include <stdarg.h>
@@ -8,49 +8,50 @@
 
 #define LINE_NCOLS 0x8		// Number of columens per line
 
-void dummy_init(void){ }
-int dummy_write(const char *str, int len){ return len; }
-int dummy_read(char *str, int len){ return 0; }
-int dummy_available(void) { return 0; }
+static int dummy_write(const char *str, int len){ return len; }
+static int dummy_read(char *str, int len){ return 0; }
+static int dummy_available(void) { return 0; }
 
-stdinout_t dummy_out = {
+static const stdinout_t dummy_out = {
     .available = dummy_available,
 	.read = dummy_read,
 	.write = dummy_write,
 };
 
-static stdinout_t *sto = &dummy_out;
+static const stdinout_t *sto = &dummy_out;
 
-void dbg_init(stdinout_t *stdo){
+void dbg_init(const stdinout_t *stdo)
+{
 	if(stdo != NULL){
 		sto = stdo;
 	}
 }
 
-void dbg_HexDumpLine(uint8_t *mem, uint32_t len, uint8_t print_ascii ){
-int i;
+void dbg_HexDumpLine(const uint8_t *mem, uint32_t len, uint8_t print_ascii)
+{
+    int i;
 
     for(i=0; i<len; i++){
 		dbg_printf("%02X ",*(mem + i));
 	}
 
-    if(!print_ascii){
-        return;
+    if(print_ascii){
+        for(i=0;i<len;i++){
+            if(*mem > (' '-1) && *mem < 0x7F)
+			dbg_printf("%c", *mem);
+            else{
+                dbg_putchar('.');
+            }
+            mem++;
+        }
 	}
 
-	for(i=0;i<len;i++){
-		if(*mem > (' '-1) && *mem < 0x7F)
-			dbg_printf("%c", *mem);
-		else{
-			dbg_putchar('.');
-		}
-		mem++;
-	}
 	dbg_putchar('\n');
 }
 
 
-void dbg_HexDump(uint8_t *mem, uint32_t len){
+void dbg_HexDump(const uint8_t *mem, uint32_t len)
+{
     //dbg_printf("\nDump address: 0x%X \n\n",(uint32_t)&mem[0]);
 	for(int i=0; i<len ;i+=LINE_NCOLS){
 		dbg_printf("%02X: ",i);
@@ -59,11 +60,13 @@ void dbg_HexDump(uint8_t *mem, uint32_t len){
 	}
 }
 
-void dbg_putchar(char c){
+void dbg_putchar(char c)
+{
     sto->write((const char*)&c, 1);
 }
 
-int dbg_puts(const char *str){
+int dbg_println(const char *str)
+{
     int len = 0;
     char dbg_out[DBG_PRINT_MAX_LEN];
 
@@ -75,7 +78,8 @@ int dbg_puts(const char *str){
 	return sto->write(dbg_out, len);
 }
 
-int dbg_printf(const char* fmt, ...){
+int dbg_printf(const char* fmt, ...)
+{
 	char dbg_out[DBG_PRINT_MAX_LEN];
 	va_list arp;
 	va_start(arp, fmt);
