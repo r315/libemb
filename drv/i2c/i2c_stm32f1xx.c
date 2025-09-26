b33770f84bccac1ce735ca2ffda67a2fe3114de2
r315 <38652@alunos.isel.ipl.pt>
2025-09-26 23:55:04 +0100

    IMP: dma transfers on stm32f1xx i2c driver

--- drv/i2c/i2c_stm32f1xx.c
+++ drv/i2c/i2c_stm32f1xx.c
@@ -1,17 +1,27 @@
 #include <stdint.h>
 #include <stddef.h>
 #include "stm32f1xx.h"
+#include "dma_stm32f1xx.h"
 #include "i2c.h"
 #include "dma.h"
 #include "clock.h"
 
 #define I2C_TIMEOUT     0x10000;
 
+enum i2c_state{
+    I2C_STATE_IDLE = 0,
+    I2C_STATE_START,
+    I2C_STATE_ADDR,
+    I2C_STATE_DATA,
+    I2C_STATE_STOP,
+};
+
 typedef struct{
     I2C_TypeDef *i2c;
-    dmatype_t dma_tx;
-    uint32_t last_error;
     void (*eot)(void);
+    uint32_t state;
+    dmatype_t dma_tx;
+    uint8_t device_addr;
 }hi2c_t;
 
 static hi2c_t hi2ca = {
@@ -119,6 +129,56 @@ static uint32_t i2c_slave_ack(I2C_TypeDef *i2c)
     return !(i2c->SR1 & I2C_SR1_AF);
 }
 
+static void i2c_eot_handler(hi2c_t *hi2c)
+{
+    I2C_TypeDef *i2c = hi2c->i2c;
+    uint32_t status = i2c->SR1;
+    uint32_t error =  status & (I2C_SR1_OVR | I2C_SR1_AF | I2C_SR1_ARLO | I2C_SR1_BERR);
+
+    if(error){
+        i2c->SR2 = i2c->SR2;
+        i2c->CR2 &= ~(I2C_CR2_ITEVTEN | I2C_CR2_DMAEN);
+        DMA_Cancel(&hi2c->dma_tx);
+        i2c_send_stop(i2c);
+        return;
+    }
+
+    switch(hi2c->state){
+    case I2C_STATE_START:
+        if(status & I2C_SR1_SB){
+            // Start sent, send addr + R/W bit (EV5)
+            i2c->DR = hi2c->device_addr;
+            hi2c->state = I2C_STATE_ADDR;
+            return;
+        }
+        // Something else
+        break;
+    case I2C_STATE_ADDR:
+        if(status & I2C_SR1_ADDR){
+            // Address sent and slave sent ACK
+            // clear ADDR flag (EV6)
+            i2c->SR2 = i2c->SR2;
+            hi2c->i2c->CR2 |= I2C_CR2_DMAEN;
+            hi2c->state = I2C_STATE_DATA;
+            return;
+        }
+        break;
+    case I2C_STATE_DATA:
+        if(status & I2C_SR1_BTF){
+            i2c_send_stop(i2c);
+            i2c->CR2 &= ~(I2C_CR2_ITEVTEN | I2C_CR2_DMAEN);
+            hi2c->state = I2C_STATE_IDLE;
+        }
+        break;
+
+    default:
+        break;
+    }
+}
+
+static void i2c_eota(void){i2c_eot_handler(&hi2ca);}
+static void i2c_eotb(void){i2c_eot_handler(&hi2cb);}
+
 /**
  * @brief
  * @param i2c
@@ -189,7 +249,31 @@ uint32_t I2C_Init(i2cbus_t *i2cbus)
 
     hi2c->i2c->CR1 = I2C_CR1_PE;
 
-    return i2c_set_speed(hi2c->i2c, i2cbus->speed, 0);
+    if(i2c_set_speed(hi2c->i2c, i2cbus->speed, 0) != I2C_OK){
+        return I2C_ERR;
+    }
+
+    if(i2cbus->cfg & I2C_CFG_DMA){
+        hi2c->dma_tx.dst = (void*)&hi2c->i2c->DR;
+        hi2c->dma_tx.dsize = DMA_CCR_PSIZE_8;
+        hi2c->dma_tx.src = NULL;
+        hi2c->dma_tx.ssize = DMA_CCR_MSIZE_8;
+        hi2c->dma_tx.dir = DMA_DIR_M2P;
+        hi2c->dma_tx.single = 1;
+        if(hi2c->i2c == I2C1){
+            hi2c->dma_tx.eot = i2c_eota;
+            DMA_Config(&hi2c->dma_tx, DMA1_REQ_I2C1_TX);
+            NVIC_EnableIRQ(I2C1_EV_IRQn);
+            NVIC_EnableIRQ(I2C1_ER_IRQn);
+        }else{
+            hi2c->dma_tx.eot = i2c_eotb;
+            DMA_Config(&hi2c->dma_tx, DMA1_REQ_I2C2_TX);
+            NVIC_EnableIRQ(I2C2_EV_IRQn);
+            NVIC_EnableIRQ(I2C2_ER_IRQn);
+        }
+    }
+
+    return I2C_OK;
 }
 
 /**
@@ -205,6 +289,7 @@ uint16_t I2C_Write(i2cbus_t *i2cbus, uint8_t addr, const uint8_t *data, uint16_t
     hi2c_t *hi2c;
     I2C_TypeDef *i2c;
     const uint8_t *end;
+    uint32_t res;
 
     if(!i2cbus){
         return 0;
@@ -214,9 +299,9 @@ uint16_t I2C_Write(i2cbus_t *i2cbus, uint8_t addr, const uint8_t *data, uint16_t
     i2c = hi2c->i2c;
 
     /* Send 8-bit slave address + W */
-    hi2c->last_error = i2c_master_send_addr(i2c, addr << 1);
+    res = i2c_master_send_addr(i2c, addr << 1);
 
-    if (hi2c->last_error != I2C_OK){
+    if (res != I2C_OK){
         i2c_send_stop(i2c);
         return 0;
     }
@@ -225,8 +310,8 @@ uint16_t I2C_Write(i2cbus_t *i2cbus, uint8_t addr, const uint8_t *data, uint16_t
 
     while (data < end){
         /* Wait until TXE flag is set (EV8)*/
-        hi2c->last_error = i2c_wait_txe(i2c);
-        if (hi2c->last_error != I2C_OK){
+        res = i2c_wait_txe(i2c);
+        if (res != I2C_OK){
             i2c_send_stop(i2c);
             return size - (end - data);
         }
@@ -266,3 +351,64 @@ void I2C_Reset(i2cbus_t *i2cbus)
     i2c->CR1 &= ~I2C_CR1_SWRST;
 }
 
+void I2C_SetEOT(i2cbus_t *i2cbus, void (*eot)(void))
+{
+    // Not checking eot for null allows disable callback
+    if(i2cbus){
+        ((hi2c_t*)i2cbus->handle)->dma_tx.eot = eot;
+    }
+}
+
+void I2C_WaitEOT(i2cbus_t *i2cbus)
+{
+    (void)i2cbus;
+}
+
+/**
+ * @brief
+ * @param i2cbus
+ * @param addr
+ * @param data
+ * @param size
+ * @return
+ */
+uint8_t dt[] = {1,2};
+uint32_t I2C_TransmitDMA(i2cbus_t *i2cbus, uint8_t addr, const uint8_t *data, uint16_t size)
+{
+
+    hi2c_t *hi2c;
+    I2C_TypeDef *i2c;
+
+    if(!i2cbus){
+        return 0;
+    }
+
+    hi2c = (hi2c_t*)i2cbus->handle;
+    i2c = hi2c->i2c;
+
+    hi2c->dma_tx.len = size;
+    hi2c->device_addr = addr << 1;
+    DMA_SetSrc(&hi2c->dma_tx, (void*)data);
+    DMA_Start(&hi2c->dma_tx);
+
+    i2c->CR2 |= (I2C_CR2_ITEVTEN);
+    hi2c->state = I2C_STATE_START;
+    i2c_send_start(i2c);
+
+    return I2C_OK;
+}
+
+
+void I2C1_EV_IRQHandler(void)
+{
+    i2c_eot_handler(&hi2ca);
+}
+
+void I2C1_ER_IRQHandler(void){i2c_eot_handler(&hi2ca);}
+
+void I2C2_EV_IRQHandler(void)
+{
+    i2c_eot_handler(&hi2cb);
+}
+
+void I2C2_ER_IRQHandler(void) {i2c_eot_handler(&hi2cb);}
--- drv/inc/i2c.h
+++ drv/inc/i2c.h
@@ -16,7 +16,11 @@ extern "C" {
 
 #include <stdint.h>
 
-enum i2cx{
+enum i2ccfg_e{
+    I2C_CFG_DMA       = (1 << 1),
+};
+
+enum i2cx_e{
     I2C_BUS0 = 0,
     I2C_BUS1,
     I2C_BUS2,
@@ -40,11 +44,12 @@ typedef struct {
     void *handle;
     uint8_t addr;       // own address
     uint32_t speed;
+    uint8_t cfg;
     uint8_t bus_num;    // Bus number
 }i2cbus_t;
 
-uint32_t I2C_Init(i2cbus_t *i2c);
-void I2C_Reset(i2cbus_t *i2c);
+uint32_t I2C_Init(i2cbus_t *i2cbus);
+void I2C_Reset(i2cbus_t *i2cbus);
 /**
  * @brief Basic I2C transfers
  *
@@ -54,8 +59,19 @@ void I2C_Reset(i2cbus_t *i2c);
  * @param size  Number of bytes in data
  * @return uint16_t number of bytes transfered
  */
-uint16_t I2C_Write(i2cbus_t *i2c, uint8_t addr, const uint8_t *data, uint16_t size);
-uint16_t I2C_Read(i2cbus_t *i2c, uint8_t addr, uint8_t *data, uint16_t size);
+uint16_t I2C_Write(i2cbus_t *i2cbus, uint8_t addr, const uint8_t *data, uint16_t size);
+uint16_t I2C_Read(i2cbus_t *i2cbus, uint8_t addr, uint8_t *data, uint16_t size);
+
+uint32_t I2C_TransmitDMA(i2cbus_t *i2cbus, uint8_t addr, const uint8_t *data, uint16_t size);
+/**
+ * @brief Configure a End Of Transfer callback.
+ * Using a separated function instead of a struct member
+ * allows setting a callback at any time by any driver/app
+ * @param i2cbus
+ * @param eot
+ */
+void I2C_SetEOT(i2cbus_t *i2cbus, void (*eot)(void));
+void I2C_WaitEOT(i2cbus_t *i2cbus);
 
 #ifdef __cplusplus
 }
