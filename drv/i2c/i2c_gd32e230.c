6acddd31578f067108fb840bca268bc0040b2a6c
r315 <38652@alunos.isel.ipl.pt>
2025-09-30 20:22:35 +0100

    FIX Build errors on gd32 i2c driver

--- drv/i2c/i2c_gd32e230.c
+++ drv/i2c/i2c_gd32e230.c
@@ -4,6 +4,8 @@
 
 #define I2Cx_OWN_ADDRESS7      0x72
 
+typedef enum i2cx_e i2cbusnum_t;
+
 enum i2c_event {
     I2C_EVENT_CHECK_NONE = 0,
     I2C_EVENT_CHECK_ACKFAIL,
@@ -11,33 +13,33 @@ enum i2c_event {
 };
 
 typedef enum i2c_status {
-    I2C_OK = 0,          /*!< no error */
-    I2C_ERR_STEP_1,      /*!< step 1 error */
-    I2C_ERR_STEP_2,      /*!< step 2 error */
-    I2C_ERR_STEP_3,      /*!< step 3 error */
-    I2C_ERR_STEP_4,      /*!< step 4 error */
-    I2C_ERR_STEP_5,      /*!< step 5 error */
-    I2C_ERR_STEP_6,      /*!< step 6 error */
-    I2C_ERR_STEP_7,      /*!< step 7 error */
-    I2C_ERR_STEP_8,      /*!< step 8 error */
-    I2C_ERR_STEP_9,      /*!< step 9 error */
-    I2C_ERR_STEP_10,     /*!< step 10 error */
-    I2C_ERR_STEP_11,     /*!< step 11 error */
-    I2C_ERR_STEP_12,     /*!< step 12 error */
-    I2C_ERR_START,       /*!< start error */
-    I2C_ERR_ADDR10,      /*!< addr10 error */
-    I2C_ERR_ADDR,        /*!< addr error */
-    I2C_ERR_STOP,        /*!< stop error */
-    I2C_ERR_ACKFAIL,     /*!< ackfail error */
-    I2C_ERR_TIMEOUT,     /*!< timeout error */
-    I2C_ERR_INTERRUPT,   /*!< interrupt error */
+    I2C_OK = 0,            /*!< no error */
+    I2C_ERROR_STEP_1,      /*!< step 1 error */
+    I2C_ERROR_STEP_2,      /*!< step 2 error */
+    I2C_ERROR_STEP_3,      /*!< step 3 error */
+    I2C_ERROR_STEP_4,      /*!< step 4 error */
+    I2C_ERROR_STEP_5,      /*!< step 5 error */
+    I2C_ERROR_STEP_6,      /*!< step 6 error */
+    I2C_ERROR_STEP_7,      /*!< step 7 error */
+    I2C_ERROR_STEP_8,      /*!< step 8 error */
+    I2C_ERROR_STEP_9,      /*!< step 9 error */
+    I2C_ERROR_STEP_10,     /*!< step 10 error */
+    I2C_ERROR_STEP_11,     /*!< step 11 error */
+    I2C_ERROR_STEP_12,     /*!< step 12 error */
+    I2C_ERROR_START,       /*!< start error */
+    I2C_ERROR_ADDR10,      /*!< addr10 error */
+    I2C_ERROR_ADDR,        /*!< addr error */
+    I2C_ERROR_STOP,        /*!< stop error */
+    I2C_ERROR_ACKFAIL,     /*!< ackfail error */
+    I2C_ERROR_TIMEOUT,     /*!< timeout error */
+    I2C_ERROR_INTERRUPT,   /*!< interrupt error */
 } i2c_status_t;
 
 typedef struct i2c_handle {
     volatile uint32_t periph;
-}i2c_handle_t;
+}hi2c_t;
 
-static i2c_handle_t hi2cx;
+static hi2c_t hi2ca = {I2C0}, hi2cb = {I2C1};
 
 static void i2c_init_gpio(i2cbusnum_t bus_num)
 {
@@ -94,7 +96,7 @@ static i2c_status_t i2c_wait_flag(uint32_t i2c_periph, uint32_t flag, uint32_t e
 
                     /* clear ack fail flag */
                     i2c_flag_clear(i2c_periph, I2C_FLAG_AERR);
-                    return I2C_ERR_ACKFAIL;
+                    return I2C_ERROR_ACKFAIL;
                 }
             }
 
@@ -121,7 +123,7 @@ static i2c_status_t i2c_send_start_and_addr(uint32_t i2c_periph, uint16_t addr, 
 {
      /* wait until I2C bus is idle */
     if (i2c_wait_flag(i2c_periph, I2C_FLAG_I2CBSY, I2C_EVENT_CHECK_NONE, timeout) != I2C_OK){
-        return I2C_ERR_STEP_1;
+        return I2C_ERROR_STEP_1;
     }
 
     /* enable ack */
@@ -132,7 +134,7 @@ static i2c_status_t i2c_send_start_and_addr(uint32_t i2c_periph, uint16_t addr, 
 
     /* wait until SBSEND bit is set */
     if(i2c_wait_flag(i2c_periph, I2C_FLAG_SBSEND, I2C_EVENT_CHECK_NONE, timeout) != I2C_OK){
-        return I2C_ERR_STEP_2;
+        return I2C_ERROR_STEP_2;
     }
 
     /* send slave address to I2C bus */
@@ -140,7 +142,7 @@ static i2c_status_t i2c_send_start_and_addr(uint32_t i2c_periph, uint16_t addr, 
 
     /* wait until ADDSEND bit is set */
     if(i2c_wait_flag(i2c_periph, I2C_FLAG_ADDSEND, I2C_EVENT_CHECK_ACKFAIL, timeout) != I2C_OK){
-        return I2C_ERR_STEP_3;
+        return I2C_ERROR_STEP_3;
     }
 
     /* clear ADDSEND bit */
@@ -152,14 +154,14 @@ static i2c_status_t i2c_send_start_and_addr(uint32_t i2c_periph, uint16_t addr, 
 i2c_status_t i2c_master_transmit(uint32_t i2c_periph, uint16_t slave, const uint8_t* pdata, uint16_t len, uint32_t timeout)
 {
     if(i2c_send_start_and_addr(i2c_periph, (slave << 1) & I2C_TRANSMITTER, I2C_ACK_DISABLE, timeout)){
-        return I2C_ERR_STEP_1;
+        return I2C_ERROR_STEP_1;
     }
 
     for(uint16_t i = 0; i < len; i++) {
         /* wait until the transmit data buffer is empty */
         if(i2c_wait_flag(i2c_periph, I2C_FLAG_TBE, I2C_EVENT_CHECK_ACKFAIL, timeout)!= I2C_OK){
             i2c_stop_on_bus(i2c_periph);
-            return I2C_ERR_STEP_4;
+            return I2C_ERROR_STEP_4;
         }
 
         /* data transmission */
@@ -168,7 +170,7 @@ i2c_status_t i2c_master_transmit(uint32_t i2c_periph, uint16_t slave, const uint
 
     if(i2c_wait_flag(i2c_periph, I2C_FLAG_BTC, 0, timeout) != I2C_OK){
         i2c_stop_on_bus(i2c_periph);
-        return I2C_ERR_STEP_5;
+        return I2C_ERROR_STEP_5;
     }
 
     /* send a stop condition to I2C bus */
@@ -180,7 +182,7 @@ i2c_status_t i2c_master_transmit(uint32_t i2c_periph, uint16_t slave, const uint
 i2c_status_t i2c_master_receive(uint32_t i2c_periph, uint16_t slave, uint8_t *pdata, uint16_t len, uint32_t timeout)
 {
     if(i2c_send_start_and_addr(i2c_periph, (slave << 1) | I2C_RECEIVER, I2C_ACK_ENABLE, timeout)){
-        return I2C_ERR_STEP_1;
+        return I2C_ERROR_STEP_1;
     }
 
     if (len == 1){
@@ -202,7 +204,7 @@ i2c_status_t i2c_master_receive(uint32_t i2c_periph, uint16_t slave, uint8_t *pd
             if (len == 1) {
                 if (i2c_wait_flag(i2c_periph, I2C_FLAG_RBNE, I2C_EVENT_CHECK_NONE, timeout) != I2C_OK) {
                     i2c_stop_on_bus(i2c_periph);
-                    return I2C_ERR_STEP_3;
+                    return I2C_ERROR_STEP_3;
                 }
                 (*pdata++) = i2c_data_receive(i2c_periph);
             }
@@ -211,7 +213,7 @@ i2c_status_t i2c_master_receive(uint32_t i2c_periph, uint16_t slave, uint8_t *pd
                 /* wait for second data byte to be transfered */
                 if (i2c_wait_flag(i2c_periph, I2C_FLAG_BTC, I2C_EVENT_CHECK_NONE, timeout) != I2C_OK) {
                     i2c_stop_on_bus(i2c_periph);
-                    return I2C_ERR_STEP_4;
+                    return I2C_ERROR_STEP_4;
                 }
                 /* Send stop condtion after second data byte*/
                 i2c_stop_on_bus(i2c_periph);
@@ -226,7 +228,7 @@ i2c_status_t i2c_master_receive(uint32_t i2c_periph, uint16_t slave, uint8_t *pd
             else {
                 if (i2c_wait_flag(i2c_periph, I2C_FLAG_BTC, I2C_EVENT_CHECK_NONE, timeout) != I2C_OK) {
                     i2c_stop_on_bus(i2c_periph);
-                    return I2C_ERR_STEP_5;
+                    return I2C_ERROR_STEP_5;
                 }
                 i2c_ack_config(i2c_periph, I2C_ACK_DISABLE);
                 (*pdata++) = i2c_data_receive(i2c_periph);
@@ -234,7 +236,7 @@ i2c_status_t i2c_master_receive(uint32_t i2c_periph, uint16_t slave, uint8_t *pd
 
                 if (i2c_wait_flag(i2c_periph, I2C_FLAG_BTC, I2C_EVENT_CHECK_NONE, timeout) != I2C_OK) {
                     i2c_stop_on_bus(i2c_periph);
-                    return I2C_ERR_STEP_6;
+                    return I2C_ERROR_STEP_6;
                 }
                 i2c_stop_on_bus(i2c_periph);
 
@@ -245,7 +247,7 @@ i2c_status_t i2c_master_receive(uint32_t i2c_periph, uint16_t slave, uint8_t *pd
         } else {
             if (i2c_wait_flag(i2c_periph, I2C_FLAG_RBNE, I2C_EVENT_CHECK_NONE, timeout) != I2C_OK) {
                 i2c_stop_on_bus(i2c_periph);
-                return I2C_ERR_STEP_7;
+                return I2C_ERROR_STEP_7;
             }
 
             (*pdata++) = i2c_data_receive(i2c_periph);
@@ -255,44 +257,48 @@ i2c_status_t i2c_master_receive(uint32_t i2c_periph, uint16_t slave, uint8_t *pd
     return I2C_OK;
 }
 
-void I2C_Init(i2cbus_t *i2c)
+uint32_t I2C_Init(i2cbus_t *i2cbus)
 {
-    switch(i2c->bus_num){
+    hi2c_t *hi2c;
+
+    switch(i2cbus->bus_num){
         case I2C_BUS0:
         case I2C_BUS2:
             rcu_periph_clock_enable(RCU_I2C0);
-            hi2cx.periph = I2C0;
+            hi2c = &hi2ca;
             break;
 
         case I2C_BUS1:
         case I2C_BUS3:
             rcu_periph_clock_enable(RCU_I2C1);
-            hi2cx.periph = I2C1;
+            hi2c = &hi2cb;
             break;
 
         default:
             return I2C_ERR_PARM;
     }
 
-    i2c_init_gpio(i2c->bus_num);
+    i2c_init_gpio(i2cbus->bus_num);
 
-    i2c_deinit(hi2cx.periph);
+    i2c_deinit(hi2c->periph);
     /* configure I2C clock */
-    i2c_clock_config(hi2cx.periph, i2c->speed, I2C_DTCY_2);
+    i2c_clock_config(hi2c->periph, i2cbus->speed, I2C_DTCY_2);
     /* configure I2C address */
-    i2c_mode_addr_config(hi2cx.periph, I2C_I2CMODE_ENABLE, I2C_ADDFORMAT_7BITS, I2Cx_OWN_ADDRESS7);
+    i2c_mode_addr_config(hi2c->periph, I2C_I2CMODE_ENABLE, I2C_ADDFORMAT_7BITS, I2Cx_OWN_ADDRESS7);
     /* enable I2C0 */
-    i2c_enable(hi2cx.periph);
+    i2c_enable(hi2c->periph);
+
+    i2cbus->handle = hi2c;
 
     return I2C_OK;
 }
 
-uint16_t I2C_Write(i2cbus_t *i2c, uint8_t addr, const uint8_t *data, uint16_t size)
+uint16_t I2C_Write(i2cbus_t *i2cbus, uint8_t addr, const uint8_t *data, uint16_t size)
 {
-    return i2c_master_transmit(hi2cx.periph, addr, data, size, 1000) != I2C_OK ? 0 : size;
+    return i2c_master_transmit(*(uint32_t*)i2cbus->handle, addr, data, size, 1000) != I2C_OK ? 0 : size;
 }
 
-uint16_t I2C_Read(i2cbus_t *i2c, uint8_t addr, uint8_t *data, uint16_t size)
+uint16_t I2C_Read(i2cbus_t *i2cbus, uint8_t addr, uint8_t *data, uint16_t size)
 {
-    return i2c_master_receive(hi2cx.periph, addr, data, size, 1000) != I2C_OK ? 0 : size;
+    return i2c_master_receive(*(uint32_t*)i2cbus->handle, addr, data, size, 1000) != I2C_OK ? 0 : size;
 }
