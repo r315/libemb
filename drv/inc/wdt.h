a1021c9fba71bbb737f998b5a98c597e1c8bb5ca
r315 <38652@alunos.isel.ipl.pt>
2025-10-11 19:37:09 +0100

    ADD: Doc comments

--- drv/inc/wdt.h
+++ drv/inc/wdt.h
@@ -7,8 +7,20 @@ extern "C" {
 
 #include <stdint.h>
 
+/**
+ * @brief Initializes watchdog timer
+ * @param timeout expire time in milliseconds
+ */
 void WDT_Init(uint32_t timeout);
+/**
+ * @brief Stop/disable watchdog
+ * @param
+ */
 void WDT_Stop(void);
+/**
+ * @brief Watchdog reset, called in application loop
+ * @param
+ */
 void WDT_Reset(void);
 
 #ifdef __cplusplus
