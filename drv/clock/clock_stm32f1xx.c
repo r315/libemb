d7a2515ead4d39333055a168a03b785b3f4d2b93
r315 <38652@alunos.isel.ipl.pt>
2026-02-21 01:59:28 +0000

    FIX: conflicted declarations

--- drv/clock/clock_stm32f1xx.c
+++ drv/clock/clock_stm32f1xx.c
@@ -2,16 +2,16 @@
 #include "clock.h"
 #include "stm32f1xx.h"
 
-const uint8_t APBPrescTable[8U] =  {0, 0, 0, 0, 1, 2, 3, 4};
+static const uint8_t APB_DivTable[8U] =  {0, 0, 0, 0, 1, 2, 3, 4};
 
 static uint32_t pclk1(void)
 {
-  return (SystemCoreClock >> APBPrescTable[(RCC->CFGR & RCC_CFGR_PPRE1) >> RCC_CFGR_PPRE1_Pos]);
+  return (SystemCoreClock >> APB_DivTable[(RCC->CFGR & RCC_CFGR_PPRE1) >> RCC_CFGR_PPRE1_Pos]);
 }
 
 static uint32_t pclk2(void)
 {
-  return (SystemCoreClock >> APBPrescTable[(RCC->CFGR & RCC_CFGR_PPRE2) >> RCC_CFGR_PPRE2_Pos]);
+  return (SystemCoreClock >> APB_DivTable[(RCC->CFGR & RCC_CFGR_PPRE2) >> RCC_CFGR_PPRE2_Pos]);
 }
 
 void CLOCK_GetAll(sysclock_t *clock)
@@ -21,17 +21,16 @@ void CLOCK_GetAll(sysclock_t *clock)
     }
 
     clock->cpu = SystemCoreClock;
-    clock->hclk = SystemCoreClock;
-    clock->pclk1 = pclk1();
-    clock->pclk2 = pclk2();
+    clock->clk1 = pclk1();
+    clock->clk2 = pclk2();
 }
 
-uint32_t CLOCK_Get(uint8_t idx)
+uint32_t CLOCK_Get(enum clocknr clock)
 {
-    switch(idx){
+    switch(clock){
         case CLOCK_CPU: return SystemCoreClock;
-        case CLOCK_PCLK1: return pclk1();
-        case CLOCK_PCLK2: return pclk2();
+        case CLOCK_CLK1: return pclk1();
+        case CLOCK_CLK2: return pclk2();
         default: break;
     }
     return 0;
